d01037abad4739d4a296d11d59976742bf84360c
Troy Gerwien <yortus@gmail.com>
2019-10-29 12:57:38 +0800

    export TypeInfo directly

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,12 @@ import {JSON} from './json';
 import {removeExcessProperties} from './remove-excess-properties';
 import {toString} from './to-string';
 import {TypeFromTypeInfo} from './type-from-type-info';
+import {TypeInfo} from './type-info';
 import * as t from './type-info';
 
 
-export {assert, getValidationErrors, is, JSON, removeExcessProperties, toString, TypeFromTypeInfo, t, ValidationErrors};
+export {assert, getValidationErrors, is, JSON, removeExcessProperties, toString, t};
+export {TypeFromTypeInfo, TypeInfo, ValidationErrors};
 
 
 // TODO: ...
