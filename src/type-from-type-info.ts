0a51f202165d16ee5ab8abeda5ddd2b2d186cd73
Troy Gerwien <yortus@gmail.com>
2020-02-24 12:38:21 +0800

    Put commonly nested types near the top
    ...to reduce nesting depth for complex types.

--- src/type-from-type-info.ts
+++ src/type-from-type-info.ts
@@ -4,20 +4,23 @@ import {TypeInfo} from './type-info';
 
 // (A) TypeOf<T> generic type operator
 export type TypeFromTypeInfo<T extends TypeInfo> = {0:
+
+    // Put commonly nested types near the top to reduce nesting depth for complex types.
+    T extends t.intersection<infer Mems> ? TypeOfIntersection<Mems> :
+    T extends t.union<infer Members> ? (Members[any] extends infer E ? (E extends TypeInfo ? TypeFromTypeInfo<E> : 0) : 0) :
+    T extends t.object<infer Props> ? TypeOfObject<Props> :
+
     T extends t.any ? any :
     T extends t.array<infer Elem> ? TypeFromTypeInfo<Elem>[] :
     T extends t.boolean ? boolean :
     T extends t.brandedString<infer Brand> ? string & {'🎫': Brand} :
     T extends t.date ? Date :
-    T extends t.intersection<infer Mems> ? TypeOfIntersection<Mems> :
     T extends t.never ? never :
     T extends t.null ? null :
     T extends t.number ? number :
-    T extends t.object<infer Props> ? TypeOfObject<Props> :
     T extends t.string ? string :
     T extends t.tuple<infer Elements> ? ({[K in keyof Elements]: Elements[K] extends TypeInfo ? TypeFromTypeInfo<Elements[K]> : 0}) :
     T extends t.undefined ? undefined :
-    T extends t.union<infer Members> ? (Members[any] extends infer E ? (E extends TypeInfo ? TypeFromTypeInfo<E> : 0) : 0) :
     T extends t.unit<infer Value> ? Value :
     T extends t.unknown ? unknown :
     never
