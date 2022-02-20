4f38b7b02a306017f9ee1dcea83dd41edd8f88b3
Troy Gerwien <yortus@gmail.com>
2022-02-20 11:45:43 +0800

    handle cyclic objects in inspect()

--- src/utils/inspect.ts
+++ src/utils/inspect.ts
@@ -1,4 +1,8 @@
 export function inspect(v: unknown): string {
+    return inspectInternal(v, new Set());
+}
+
+function inspectInternal(v: unknown, seen: Set<unknown>): string {
     if (v === null || v === undefined || typeof v === 'number' || typeof v === 'boolean') {
         return String(v);
     }
@@ -11,10 +15,15 @@ export function inspect(v: unknown): string {
         return v.toISOString();
     }
 
+    if (seen.has(v)) {
+        return '[cyclic]';
+    }
+
     if (typeof v === 'object' && !Array.isArray(v)) {
+        seen.add(v);
         const obj = v as Record<string, unknown>;
         const props = Object.keys(obj).map(propName => {
-            const value = inspect(obj[propName]);
+            const value = inspectInternal(obj[propName], seen);
             if (!/^[a-z_$][a-z0-9_$]*$/ig.test(propName)) propName = JSON.stringify(propName);
             return `${propName}: ${value}`;
         });
@@ -22,7 +31,8 @@ export function inspect(v: unknown): string {
     }
 
     if (typeof v === 'object' && Array.isArray(v)) {
-        return `[${v.map(elem => inspect(elem)).join(', ')}]`;
+        seen.add(v);
+        return `[${v.map(elem => inspectInternal(elem, seen)).join(', ')}]`;
     }
 
     return `[${(v as any).constructor.name}]`;
